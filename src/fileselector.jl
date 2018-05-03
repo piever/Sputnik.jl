46bd357d6375460877a99370d307e5b9d5861846
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-05-03 14:24:20 +0100

    wip2

--- src/fileselector.jl
+++ src/fileselector.jl
@@ -1,6 +1,15 @@
 o = Observable{Any}("")
 on(println, o) # print to console on every update
 data = Dict("filename" => o )
+
+s = """function (event){
+    var filePath = this.\$refs.data;
+    var fn = filePath.files[0];
+    return this.filename = fn.name
+}
+"""
+jfunc = WebIO.JSString(s)
+
 ui = vue(dom"input[ref=data, type=file, id=input, v-on:change=onFileChange]"(),
-    data, methods = Dict(:onFileChange =>
-        WebIO.JSString("(function (event){return this.filename = this.\$refs.data.files[0].name})"))) |> dom"div"
+    data, methods = Dict(:onFileChange => jfunc)) |> dom"div"
+webio_serve(page("/", req ->ui))
