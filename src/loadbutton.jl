4c8e99c4dfd15b5f79f80693541513340eb6b584
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-05-07 15:30:33 +0100

    improved loadbutton

--- src/loadbutton.jl
+++ src/loadbutton.jl
@@ -1,21 +1,9 @@
 function loadbutton()
-    o = Observable{Any}("")
+    s = choosefile(placeholder="Load file", accept=".csv")
+    o = observe(s)
     on(launch, o) # print to console on every update
-    data = Dict("filename" => o )
-
-    s = """function (event){
-        var filePath = this.\$refs.data;
-        var fn = filePath.files[0];
-        return this.filename = fn.path
-    }
-    """
-    jfunc = WebIO.JSString(s)
-
-    ui = vue(dom"input[ref=data, type=file, id=input, v-on:change=onFileChange]"(),
-        data, methods = Dict(:onFileChange => jfunc))
-
     tables = vcat("", readdir(tablefolder))
-    d = dropdown(tables)
+    d = dropdown(tables, label="saved")
     on(x -> x!="" && launch(JuliaDB.load(joinpath(tablefolder, x))), observe(d))
-    hbox(pad((:top, :left), 20px, ui), d) |> dom"div"
+    hbox(d, hskip(20px), s)
 end
