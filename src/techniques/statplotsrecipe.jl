091c4a5a1d3fa2cf128d4bc73dcd3ed087eb1ba8
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-06-07 14:53:31 +0100

    add binning to statistical recipes

--- src/techniques/statplotsrecipe.jl
+++ src/techniques/statplotsrecipe.jl
@@ -3,5 +3,9 @@ function process(::Type{StatPlotsRecipe}, a::Analysis)
     t = a.data.table
     grp_cols = splitby(a)
     cols = select(t, All(args, grp_cols)) |> dropna
-    a.plot(columns(cols, args)...; group = columns(cols, grp_cols),  get_style(a.data)..., a.plot_kwargs...)
+    a.plot(columns(cols, args)...;
+        nbins = round(Int64, (120-a.smoother)/2),
+        group = columns(cols, grp_cols),
+        get_style(a.data)...,
+        a.plot_kwargs...)
 end
